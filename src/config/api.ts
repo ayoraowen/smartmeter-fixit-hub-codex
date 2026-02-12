86a6b5b5077b761f5c30d8b95c0f287fe3574991
Owen Ayora <ayoraowen@gmail.com>
2026-02-12 23:39:47 +0300

    Substituting deployed API_BASE_URL with local dev api url for local dev testing, deployed base url commented out

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "https://localhost:3000" //import.meta.env.VITE_API_BASE_URL;
 
 if (!API_BASE_URL) {
   console.warn("⚠️ VITE_API_BASE_URL is not defined");
